77d5a65811bab8a6c7d57ff8347f2b31032a066a
InfectionMushrooms <creatortag21@gmail.Com>
2015-01-25 13:43:16 +0100

    Firefox bug

--- Chun AITD forum.user.js
+++ Chun AITD forum.user.js
@@ -5,6 +5,7 @@
 // @description  Such a useful description aiiight mah nigga
 // @author       Infection
 // @include      http://mush.vg/g/chun-alone-in-the-dark/forum/*
+// @grant        XMlHttpRequest
 // @include		 http://chun-alone-in-the-dark.xooit.org/*
 // @require http://ajax.googleapis.com/ajax/libs/jquery/1/jquery.min.js
 // ==/UserScript==
@@ -24,33 +25,34 @@ function createIframe(){
   document.getElementById("mush_content").appendChild(i);
 };
 	
-// Check for browser support of event handling capability
-if (window.addEventListener)
-window.addEventListener("load", createIframe, false);
-else if (window.attachEvent)
-window.attachEvent("onload", createIframe);
-else window.onload = createIframe;
 
-$("#chunXooit").attr("frameborder","0");
+
 
 if (window.top === window.self) {
-    // main site...
+	// Check for browser support of event handling capability
+    
+	if (window.addEventListener)
+		window.addEventListener("load", createIframe, false);
+	else if (window.attachEvent)
+		window.attachEvent("onload", createIframe);
+	else window.onload = createIframe;
+    
+	$("#chunXooit").attr("frameborder","0");
+	$("#maincontainer").append('<img id="icoCXooit" src="http://imgup.motion-twin.com/twinoid/e/6/10e87e1a_124312.jpg" style="position: fixed;bottom:0; right: 0">');
+
+	$(document).on("click", "#icoCXooit", function(e) {
+		$("#maincontainer").find('.tid_module').slideToggle("slow");
+		$("iframe").slideToggle("slow");
+	});
+
+	$('#icoCXooit').hover(function() {
+    	$(this).css('cursor','pointer');
+	});
 }
 else {
     //iframe...
 
 }
-
-$("#maincontainer").append('<img id="icoCXooit" src="http://imgup.motion-twin.com/twinoid/e/6/10e87e1a_124312.jpg" style="position: fixed;bottom:0; right: 0">');
-
-$(document).on("click", "#icoCXooit", function(e) {
-	$("#maincontainer").find('.tid_module').slideToggle("slow");
-	$("iframe").slideToggle("slow");
-});
-
-$('#icoCXooit').hover(function() {
-    $(this).css('cursor','pointer');
-});
 
 
 
