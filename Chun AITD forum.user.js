e2ad90c95122f59c5a4b339ed8ca96349ea969f7
InfectionMushrooms <creatortag21@gmail.Com>
2015-01-27 20:55:28 +0100

    Clean

--- Chun AITD forum.user.js
+++ Chun AITD forum.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         Chun, AITD forum
 // @namespace    http://your.homepage/
-// @version      0.00004
+// @version      0.00006
 // @description  Such a useful description aiiight mah nigga
 // @author       Infection
 // @include      http://mush.vg/*
@@ -10,6 +10,20 @@
 // @require http://ajax.googleapis.com/ajax/libs/jquery/1/jquery.min.js
 // ==/UserScript==
 
+var notifXooit = [];
+
+function makeIco(){
+    $("#notifXooit").remove();
+    $("#maincontainer").append('<div id="notifXooit"><img id="icoCXooit" src="http://imgup.motion-twin.com/twinoid/e/6/10e87e1a_124312.jpg" style="position: fixed;bottom:0; right: 0"></div>');
+    	$(document).on("click", "#icoCXooit", function(e) {
+		$("#maincontainer").find('.tid_module').slideToggle("slow");
+		$("iframe").slideToggle("slow");
+	});
+
+	$('#icoCXooit').hover(function() {
+    	$(this).css('cursor','pointer');
+	});
+}
 
 function createIframe(){
   var i = document.createElement("iframe");
@@ -36,20 +50,12 @@ if ((window.top === window.self) && (window.location.href.indexOf("118711") > -1
 	else window.onload = createIframe;
     
 	$("#chunXooit").attr("frameborder","0");
-	$("#maincontainer").append('<img id="icoCXooit" src="http://imgup.motion-twin.com/twinoid/e/6/10e87e1a_124312.jpg" style="position: fixed;bottom:0; right: 0">');
-
-	$(document).on("click", "#icoCXooit", function(e) {
-		$("#maincontainer").find('.tid_module').slideToggle("slow");
-		$("iframe").slideToggle("slow");
-	});
-
-	$('#icoCXooit').hover(function() {
-    	$(this).css('cursor','pointer');
-	});
+	makeIco();
 }
 else {
     //iframe...	
     if ((window.location.href.indexOf("xooit") > -1) && !(window.top === window.self)){
+        
     if (window.addEventListener)
         window.addEventListener("load", hideUselessStuff, false);
 	else if (window.attachEvent)
@@ -66,8 +72,33 @@ function hideUselessStuff(){
     $("body").find("table").first().attr("width","100%");
     $("td.gensmall").append('<br /><a href="http://chun-alone-in-the-dark.xooit.org/privmsg.php?folder=inbox" \
 							class="gensmall">Boite de réception</a>');
+    $("body#check").css("color","#EBEBEC");
+}
+
+             
+function checkNewMessages(){
+    $.ajax({
+      type:     "GET",
+      url:      "http://chun-alone-in-the-dark.xooit.org/search.php?search_id=egosearch",
+      dataType: "html",
+      success: function(data){
+          parsedElements = $(data);
+          var nbrNotif = 0;
+          parsedElements.find(".bodyline").find(".forumline").find("tr").each(function(){
+              if ($(this).find("td").first().find("img").attr("alt") == "Nouveaux messages"){
+                  nbrNotif++;
+                  notifXooit[nbrNotif] = $(this).find("td").eq(1).find("a").text();
+              }
+          });
+      }
+    });
 }
 
 
-
+$(window).on('hashchange', function(e){
+    if(window.location.href.indexOf("118711") > -1){
+        makeIco();
+        createIframe();
+    }
+});
 
